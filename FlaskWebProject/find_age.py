ff8cee22dc14e10a0eeecedbb37d6e8d7bf689ef
Anna <anya.rezistor@gmail.com>
2015-11-20 18:11:41 +0300

    test Find_age

--- FlaskWebProject/find_age.py
+++ FlaskWebProject/find_age.py
@@ -1,7 +1,7 @@
-#import numpy as np
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#import networkx as nx
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
 import os
 #from collections import Counter
 #import datetime as dt
