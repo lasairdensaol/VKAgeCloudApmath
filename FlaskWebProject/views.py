49b9bc4ee2d8520c4b9dc795c45a248ca4dc9d18
Anna <anya.rezistor@gmail.com>
2015-11-20 17:46:23 +0300

    test views

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -5,39 +5,28 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template, request
 from FlaskWebProject import app
-import numpy as np
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
 	results = {}
+	age = [1,3,4,2,4]
 	errors = []
 	if request.method == 'POST':
-		user_id = request.form['id']
-		if user_id:
-			results = [('Mi', np.random.randint(100))]
-			return render_template('index.html', errors = errors, user_id = user_id, results = results)
-		else:
-			errors.append("The form should not be empty")
+		try:
+			user_id = request.form['id']
+			if user_id:
+				###age = find_age.find_user_age(int(user_id))
+				results  = [('Method 1', age[0]),
+							('Method 2', age[1]),
+							('Method 3', age[2]),
+							('Method 4', age[3]),
+							('Method 5', age[4])]
+				return render_template('index.html', errors = errors, user_id = user_id, results = results)
+			else:
+				errors.append("The form should not be empty")
+				return render_template('index.html', errors = errors)
+		except:
+			errors.append("Wrong id or no access the Internet. Please make sure it is right and try again")
 			return render_template('index.html', errors = errors)
-		
-		###try:
-			###user_id = request.form['id']
-			###mynum = np.random.randint(100)
-			###errors.append(str(mynum))
-			###if user_id:
-			###	errors.append("Mi")
-			###	age = find_age.find_user_age(int(user_id))
-			###	results  = [('Method 1', age[0]),
-			###				('Method 2', age[1]),
-			###				('Method 3', age[2]),
-			###				('Method 4', age[3]),
-			###				('Method 5', age[4])]
-			###	return render_template('index.html', errors = errors, user_id = user_id, results = results)
-			###else:
-			###	errors.append("The form should not be empty")
-			###	return render_template('index.html', errors = errors)
-		###except:
-			###errors.append("Wrong id or no access the Internet. Please make sure it is right and try again")
-			###return render_template('index.html', errors = errors)
 
 	return render_template('index.html')
