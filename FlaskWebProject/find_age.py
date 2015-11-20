29a087e24e7b670acbe0493095220764ffdc85e8
Anna <anya.rezistor@gmail.com>
2015-11-20 18:10:32 +0300

    test Find_age

--- FlaskWebProject/find_age.py
+++ FlaskWebProject/find_age.py
@@ -1,11 +1,11 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import networkx as nx
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#import networkx as nx
 import os
-from collections import Counter
-import datetime as dt
-import community
+#from collections import Counter
+#import datetime as dt
+#import community
 
 ###from FlaskWebProject import vk_api
 #
