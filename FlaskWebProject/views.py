84ea6307f3e35eae006da0484139510921f57ffe
Anna <anya.rezistor@gmail.com>
2015-11-20 14:09:35 +0300

    change views and templates

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -6,32 +6,29 @@ from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
 
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+	###results = {}
+	###errors = []
+	###if request.method == 'POST':
+		###try:
+			###user_id = request.form['id']
+			###mynum = np.random.randint(100)
+			###errors.append(str(mynum))
+			###if user_id:
+			###	errors.append("Mi")
+			###	age = find_age.find_user_age(int(user_id))
+			###	results  = [('Method 1', age[0]),
+			###				('Method 2', age[1]),
+			###				('Method 3', age[2]),
+			###				('Method 4', age[3]),
+			###				('Method 5', age[4])]
+			###	return render_template('index.html', errors = errors, user_id = user_id, results = results)
+			###else:
+			###	errors.append("The form should not be empty")
+			###	return render_template('index.html', errors = errors)
+		###except:
+			###errors.append("Wrong id or no access the Internet. Please make sure it is right and try again")
+			###return render_template('index.html', errors = errors)
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
+	return render_template('index.html')
